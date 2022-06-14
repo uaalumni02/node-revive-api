7e9506c29f3ad67bca01270b787a1d927fb67e15
De'Meco Bell <dbell1911@gmail.com>
2022-06-13 21:09:57 -0400

    server setup

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config'
+import cors from 'cors';
+
+
+const app = express();
+const { log, error } = console;
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(port, () => log('server is running'));
+export default app;
